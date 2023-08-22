63ca9fd6e3245af6592e08c7186e199492ae9c9d
Liz Fedak <liz@lizfedak.com>
2023-08-22 13:00:24 -0600

    escape chars

--- src/app/Headersection.js
+++ src/app/Headersection.js
@@ -5,8 +5,8 @@ export default function Headersection() {
     return (
         <section className="my-20 flex flex-col gap-5">
             <div className="wave w-fit text-5xl">👋</div>
-            <div className="text-3xl">Hi there! I'm Liz, a developer based in Boulder, CO.</div>
-            <div>I'm an expert JavaScript developer experienced in crafting responsive web plugins, business automations, and API integrations.
+            <div className="text-3xl">Hi there! I\'m Liz, a developer based in Boulder, CO.</div>
+            <div>I\'m an expert JavaScript developer experienced in crafting responsive web plugins, business automations, and API integrations.
             </div>
             <button className="my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit">
                 <Link href="/about">About Me</Link>
