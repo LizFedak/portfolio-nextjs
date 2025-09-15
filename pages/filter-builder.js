a67400d204f7469b931ba391260f41a55ba10027
LizFedak <liz@lizfedak.com>
2025-09-15 13:23:53 -0700

    Update filter-builder.js

--- pages/filter-builder.js
+++ pages/filter-builder.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function CustomAuthenticationSuite() {
   return (
-    <div className="p-6 bg-white rounded-2xl shadow">
-      <h2 className="text-xl font-semibold mb-2">View site page at widgetpro.io</h2>
-      
+    <div>
+      <h2>View site page at widgetpro</h2>
       </div>
-    </div>
   );
 }
